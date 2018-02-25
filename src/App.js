4d149eb9fd45658c1d411c433196e171b4b19547
Daniel Duncan <dduncan@medallia.com>
2018-02-25 14:35:21 -0800

    Completes lecture 39

--- src/App.js
+++ src/App.js
@@ -23,11 +23,15 @@ class App extends Component {
         ]
     };
 
+    switchNameHandler = () => {
+        console.log('was clicked');
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App!</h1>
-                <button>Switch Name</button>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.people[0].name} age={this.state.people[0].age}>{this.state.people[0].hobbies === undefined || this.state.people[0].hobbies.length === 0 ? '' : 'My Hobbies: ' + this.state.people[0].hobbies.join(',')}</Person>
                 <Person name={this.state.people[1].name} age={this.state.people[1].age}>{this.state.people[1].hobbies === undefined || this.state.people[1].hobbies.length === 0  ? '' : 'My Hobbies: ' + this.state.people[1].hobbies.join(', ')}</Person>
                 <Person name={this.state.people[2].name} age={this.state.people[2].age}>{this.state.people[2].hobbies === undefined || this.state.people[2].hobbies.length === 0  ? '' : 'My Hobbies: ' + this.state.people[2].hobbies.join(', ')}</Person>
