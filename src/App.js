9ee09669f08c28afa98b8949e7e014738130622d
Daniel Duncan <dduncan@medallia.com>
2018-03-14 20:25:31 -0700

    Completes lecture 62

--- src/App.js
+++ src/App.js
@@ -57,7 +57,8 @@ class App extends Component {
 
     render() {
         const buttonStyle = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
@@ -86,6 +87,7 @@ class App extends Component {
                     }
                 </div>
             );
+            buttonStyle.backgroundColor = 'red';
         }
 
         return (
